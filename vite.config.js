cd921d4e385906f380e12bd2fe25328152a3afca
unknown <noufalniyas05@gmail.com>
2025-08-30 09:22:53 +0530

    viteconfig changed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"https://github.com/Niyasnoufal/MyReactApp.git",
+  base:"/MyReactApp/",
   plugins: [
     react(),
     tailwindcss()
